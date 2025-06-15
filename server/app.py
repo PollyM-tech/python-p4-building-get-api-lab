77636b66dba0d76277555d9f3b0f0f86760f6dc8
Pauline Moraa <pmoraa431@gmail.com>
2025-06-15 05:47:17 +0300

    building a GET API

--- server/app.py
+++ server/app.py
@@ -20,19 +20,29 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(jsonify(all_bakeries), 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    if bakery:
+        return make_response(jsonify(bakery.to_dict()), 200)
+    else:
+        return make_response(jsonify({'error': 'Bakery not found'}), 404)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    goods = [bg.to_dict() for bg in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    return make_response(jsonify(goods), 200)
+   
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
-
+    bg = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if bg:
+        return make_response(jsonify(bg.to_dict()), 200)
+    else:
+        return make_response(jsonify({'error': 'No baked goods found'}), 404)
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
